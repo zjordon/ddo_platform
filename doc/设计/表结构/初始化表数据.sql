bb69007d2b474749e8836b7addd810034f1d25c4
z_jordon <z_jordon@163.com>
2015-05-05 17:20:03 +0800

    整理初始化表结构的脚本

---
+++ doc/设计/表结构/初始化表数据.sql
@@ -0,0 +1,17 @@
+truncate table ddo_repeat_msg_record;
+truncate table ddo_msg;
+truncate table ddo_repeat_report_record;
+truncate table ddo_bill_report;
+truncate table ddo_request_msisdn;
+truncate table ddo_channel_request;
+
+truncate table ddo_exist_full_msisdn;
+truncate table ddo_exist_channel_msisdn;
+truncate table ddo_send_record;
+truncate table ddo_send_result_record;
+truncate table ddo_bill_result_record;
+truncate table ddo_channel_statistics_day;
+truncate table ddo_full_statistics_day;
+
+truncate table ddo_channel_complaint_day;
+truncate table ddo_full_complaint_day;
