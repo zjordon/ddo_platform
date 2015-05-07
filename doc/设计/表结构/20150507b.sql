4d281784733b0907fa432144d45136969639426a
z_jordon <z_jordon@163.com>
2015-05-07 17:44:07 +0800

    全量统计和按渠道统计改成按月统计

---
+++ doc/设计/表结构/20150507b.sql
@@ -0,0 +1,43 @@
+--
+-- ER/Studio 8.0 SQL Code Generation
+-- Company :      Lenovo
+-- Project :      统计模型.dm1
+-- Author :       jason
+--
+-- Date Created : Thursday, May 07, 2015 15:21:19
+-- Target DBMS : MySQL 5.x
+--
+
+-- 
+-- TABLE: ddo_channel_statistics_month 
+--
+
+CREATE TABLE ddo_channel_statistics_month(
+    id                  VARCHAR(32)      NOT NULL,
+    sum_month           INT              NOT NULL,
+    channel_id          VARCHAR(32)      NOT NULL,
+    msisdn_num          INT              NOT NULL,
+    sum_amount          DOUBLE(18, 0)    NOT NULL,
+    msg_num             INT              NOT NULL,
+    send_success_num    INT              NOT NULL,
+    send_fail_num       INT              NOT NULL,
+    bill_success_num    INT              NOT NULL,
+    bill_fail_num       INT,
+    PRIMARY KEY (id)
+)ENGINE=INNODB
+COMMENT='分渠道统计按月'
+;
+
+-- 
+-- TABLE: ddo_exist_cmsisdn_month 
+--
+
+CREATE TABLE ddo_exist_cmsisdn_month(
+    msisdn        BIGINT         NOT NULL,
+    channel_id    VARCHAR(32)    NOT NULL,
+    sum_month     INT            NOT NULL,
+    PRIMARY KEY (msisdn, channel_id, sum_month)
+)ENGINE=INNODB
+COMMENT='已统计渠道号码按月'
+;
+
