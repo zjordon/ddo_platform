0a6874876a28ed791e9f9824d0282597046c1290
z_jordon <z_jordon@163.com>
2015-06-09 11:37:30 +0800

    临时增加对任广伟这个渠道的判断

--- ddoMsg/src/main/java/com/jason/ddoMsg/externalInterface/DdoMsgInterface.java
+++ ddoMsg/src/main/java/com/jason/ddoMsg/externalInterface/DdoMsgInterface.java
@@ -77,6 +77,10 @@ public class DdoMsgInterface {
 					//System.out.println("asyncNotifyURL is " + asyncNotifyURL);
 					String mobile = Long.toString(ddoMsg.getMsisdn().longValue());
 					String channelId = configCache.getDdoChannelId();
+					//临时判断任广伟这个渠道，只有这个渠道才提交渠道id
+					if (!"ff8080814d9e79d8014d9edd8add00cc".equals(ddoMsg.getChannelId())) {
+						channelId = "";
+					}
 //					if (StringUtils.isBlank(channelId)) {
 //						channelId = "";
 //					}
